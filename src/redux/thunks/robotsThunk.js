15dad42b23ab8602dc5849370fcaee43db10619b
Alejandro Rodríguez <a.rodriguezmachin@gmail.com>
2022-02-19 17:22:04 +0100

    changed path in fetch to match with the one in the api

--- src/redux/thunks/robotsThunk.js
+++ src/redux/thunks/robotsThunk.js
@@ -1,7 +1,7 @@
 import { loadAllRobotsAction } from "../actions/actionCreator";
 
 export const loadAllRobotsThunk = async (dispatch) => {
-  const response = await fetch(process.env.REACT_APP_DEVURL);
+  const response = await fetch(`${process.env.REACT_APP_DEVURL}/`);
 
   const robots = await response.json();
 

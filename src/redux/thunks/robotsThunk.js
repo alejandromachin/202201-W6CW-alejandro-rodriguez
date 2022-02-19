04b19887af42dc27011c450ca943712d9ad93b1d
Alejandro Rodríguez <a.rodriguezmachin@gmail.com>
2022-02-19 17:14:34 +0100

    added env variables and changed fetch to load

--- src/redux/thunks/robotsThunk.js
+++ src/redux/thunks/robotsThunk.js
@@ -1,11 +1,7 @@
 import { loadAllRobotsAction } from "../actions/actionCreator";
 
 export const loadAllRobotsThunk = async (dispatch) => {
-  const response = await fetch(
-    process.env.REACT_APP_PRODUCTIONURL
-      ? `${process.env.REACT_APP_PRODUCTIONURL}robots`
-      : `${process.env.REACT_APP_DEVURL}robots`
-  );
+  const response = await fetch(process.env.REACT_APP_DEVURL);
 
   const robots = await response.json();
 

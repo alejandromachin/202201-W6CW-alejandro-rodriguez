535fc146644cbf35aa676aed9677ea5e0cca1f2e
Alejandro Rodríguez <a.rodriguezmachin@gmail.com>
2022-02-19 11:18:22 +0100

    rendered a list of robots in robotlistcomponent

--- src/components/RobotListComponent/RobotListComponent.js
+++ src/components/RobotListComponent/RobotListComponent.js
@@ -1,5 +1,16 @@
-const RobotListComponent = () => {
-  return <></>;
+import RobotComponent from "../RobotComponent/RobotComponent";
+
+const RobotListComponent = ({ robots }) => {
+  return (
+    <>
+      <ul>
+        {robots.map((robot) => (
+          <RobotComponent key={robot.id} robot={robot} />
+        ))}
+      </ul>
+      ;
+    </>
+  );
 };
 
 export default RobotListComponent;

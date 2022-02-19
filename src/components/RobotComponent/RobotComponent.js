ec19ddd07bb3824016ae6167e843bbdfe9b197ac
Alejandro Rodríguez <a.rodriguezmachin@gmail.com>
2022-02-19 10:40:48 +0100

    added information to render in robot component

--- src/components/RobotComponent/RobotComponent.js
+++ src/components/RobotComponent/RobotComponent.js
@@ -1,5 +1,17 @@
-const RobotComponent = () => {
-  return <></>;
+const RobotComponent = (robot) => {
+  return (
+    <>
+      <li>
+        <div>
+          <img src={robot.image} alt={robot.name} width="200" height="100" />
+          <h2>Name: {robot.name}</h2>
+          <p>Velocity: {robot.velocity}</p>
+          <p>Resistance: {robot.resistance}</p>
+          <p>Date of Creation: {robot.dateOfCreation}</p>
+        </div>
+      </li>
+    </>
+  );
 };
 
 export default RobotComponent;

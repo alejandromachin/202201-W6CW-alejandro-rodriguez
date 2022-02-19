f4b61cfd9e9aac061a846298434cf5cb49c45d35
Alejandro Rodríguez <a.rodriguezmachin@gmail.com>
2022-02-19 11:21:20 +0100

    added provider in index.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
+import RobotListComponent from "./components/RobotListComponent/RobotListComponent";
+
 function App() {
-  return <>
-  </>;
+  return (
+    <>
+      <h1>ROBOTS:</h1>
+      <RobotListComponent />
+    </>
+  );
 }
 
 export default App;

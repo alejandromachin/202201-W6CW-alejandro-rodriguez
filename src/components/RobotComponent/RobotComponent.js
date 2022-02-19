d27b7ebfa23a2791d38236d778475504ec596670
Alejandro Rodríguez <a.rodriguezmachin@gmail.com>
2022-02-19 17:01:40 +0100

    removed env

--- src/components/RobotComponent/RobotComponent.js
+++ src/components/RobotComponent/RobotComponent.js
@@ -8,7 +8,7 @@ const RobotComponent = ({ robot }) => {
           <img src={robot.image} alt={robot.name} width="200" height="100" />
           <h2>Name: {robot.name}</h2>
           <p>Velocity: {robot.velocity}</p>
-          <p>Resistance: {robot.resistance}</p>
+          <p>Resistance: {robot.resistency}</p>
           <p>Date of Creation: {robot.dateOfCreation}</p>
         </div>
       </li>

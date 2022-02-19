43928a77b572428fa245f558a1e831553c400355
Alejandro Rodríguez <a.rodriguezmachin@gmail.com>
2022-02-19 12:24:19 +0100

    added handlers to test the fetch in robotsThunks

--- src/redux/thunks/robotsThunk.js
+++ src/redux/thunks/robotsThunk.js
@@ -1,5 +1,9 @@
 export const loadAllRobotsThunk = async (dispatch) => {
-  const response = await fetch(`${process.env.REACT_APP_PRODUCTIONURL}robots`);
+  const response = await fetch(
+    process.env.REACT_APP_PRODUCTIONURL
+      ? `${process.env.REACT_APP_PRODUCTIONURL}robots`
+      : `${process.env.REACT_APP_DEVURL}robots`
+  );
   const robots = await response.json();
   dispatch(loadAllRobotsThunk(robots));
 };

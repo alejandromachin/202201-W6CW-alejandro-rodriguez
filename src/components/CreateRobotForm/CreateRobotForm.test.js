e8efa3a5d21646bc1607b93e4e6f7c263bc9d757
Alejandro Rodríguez <a.rodriguezmachin@gmail.com>
2022-02-20 20:28:52 +0100

    added test to check rendering of the page

---
+++ src/components/CreateRobotForm/CreateRobotForm.test.js
@@ -0,0 +1,21 @@
+import { screen } from "@testing-library/react";
+import renderWithProviders from "../../setupTests";
+import CreateRobotForm from "./CreateRobotForm";
+import userEvents from "@testing-library/user-event";
+
+describe("Given a CreateRobotForm component", () => {
+  describe("When it is instantiated", () => {
+    test("Then it should show 2 textbox inputs and a button with the text add to submit", () => {
+      const expectedNumberOfInputs = 2;
+
+      renderWithProviders(<CreateRobotForm />);
+
+      const inputs = screen.getAllByRole("textbox").length;
+      const submitButton = screen.getByRole("button", { name: /add/i });
+      userEvents.type(inputs);
+
+      expect(inputs).toBe(expectedNumberOfInputs);
+      expect(submitButton).toBeInTheDocument();
+    });
+  });
+});

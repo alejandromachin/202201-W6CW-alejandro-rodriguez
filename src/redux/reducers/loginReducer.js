637f9cfd7a206500f6f5b99444c537a1fa03abe6
Alejandro Rodríguez <a.rodriguezmachin@gmail.com>
2022-02-20 21:26:56 +0100

    created a reducer for login info

---
+++ src/redux/reducers/loginReducer.js
@@ -0,0 +1,16 @@
+import actionTypes from "../actions/actionTypes";
+
+const robotsReducer = (currentToken = [], action = {}) => {
+  let newToken;
+  switch (action.type) {
+    case actionTypes.getLogin:
+      newToken = { ...action.token };
+      break;
+
+    default:
+      newToken = [...currentToken];
+  }
+  return newToken;
+};
+
+export default robotsReducer;

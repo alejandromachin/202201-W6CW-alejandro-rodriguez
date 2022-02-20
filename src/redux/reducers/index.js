9f5079ac96061c2a7d6b4c464fbda0c4c6d92111
Alejandro Rodríguez <a.rodriguezmachin@gmail.com>
2022-02-20 21:31:52 +0100

    added token to the state with the loginReducer

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
+import loginReducer from "./loginReducer";
 import robotsReducer from "./robotsReducer";
 
 const rootReducer = combineReducers({
   robots: robotsReducer,
+  token: loginReducer,
 });
 
 export default rootReducer;

15425b306e6dec7d1e87f6a36656778bb2ee5242
Alejandro Rodríguez <a.rodriguezmachin@gmail.com>
2022-02-20 16:46:28 +0100

    added CreateRobotFormPage page to render the form

---
+++ src/pages/CreateRobotFormPage.js
@@ -0,0 +1,7 @@
+import CreateRobotForm from "../components/CreateRobotForm/CreateRobotForm";
+
+const CreateRobotFormPage = () => {
+  return <CreateRobotForm />;
+};
+
+export default CreateRobotFormPage;

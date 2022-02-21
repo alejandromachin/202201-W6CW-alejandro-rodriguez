7752cd857b9a5cdd4d6efbfb66cf8de9945a59fd
Alejandro Rodríguez <a.rodriguezmachin@gmail.com>
2022-02-21 21:35:45 +0100

    added flex style to list

--- src/components/RobotListComponent/RobotListComponent.js
+++ src/components/RobotListComponent/RobotListComponent.js
@@ -1,12 +1,20 @@
+import styled from "styled-components";
 import RobotComponent from "../RobotComponent/RobotComponent";
-
+const RobotListContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  gap: 10px;
+`;
 const RobotListComponent = ({ robots }) => {
   return (
     <>
       <ul>
-        {robots.map((robot) => (
-          <RobotComponent robot={robot} key={robot._id} />
-        ))}
+        <RobotListContainer>
+          {robots.map((robot) => (
+            <RobotComponent robot={robot} key={robot._id} />
+          ))}
+        </RobotListContainer>
       </ul>
       ;
     </>
